d789c2879f60362af260fefbb6e2b10bac17c820
Juan Gaviria <juancamilogaviriaacosta@gmail.com>
2026-05-15 23:10:13 -0500

    Commit juan 1778904613

--- four-tails-fe/src/app/app.ts
+++ four-tails-fe/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('four-tails-fe');
+  protected readonly title = signal('Four Tails');
 }
